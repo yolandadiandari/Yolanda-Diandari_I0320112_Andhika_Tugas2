697544d74811458a05dbaaa31b10ef6b68d7bcff
Yolanda Diandari <yolandadiandari@gmail.com>
2021-03-11 21:07:13 +0700

    update tugas 2

--- I0320112_exercise2.4.py
+++ I0320112_exercise2.4.py
@@ -1,1 +1,14 @@
+#menampilkan informasi program
+
+print("Konversi suhu (Fahrenheit ke Celcius)")
+
+#input suhu dalam fahrenheit
+F = float(input("Masukkan suhu(fahrenheit): "))
+
+#melakukan konversi suhu
+C = 5 * (F-32)/9
+
+#menampilkan hasil konversi ke layar
+print("Fahrenheit: ", F)
+print("Celcius:",C)
 
